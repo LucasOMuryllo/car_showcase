f369ceae37d222b014596c1eda9de55cefd1e024
Lucas <lumuryllo86@gmail.com>
2023-09-11 21:48:15 -0300

    :sparkles: create Header navbar and footer

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@
 import { CustomButtonProps } from '@/types'
 import Imagem from 'next/image'
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
     return (
         <button
             disabled={false}
-            type={'button'}
+            type={btnType ||'button'}
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
         >
